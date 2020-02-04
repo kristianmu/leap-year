899d36186bf74f3c5e9d8a19b35040227a7cec88
Kristian Muñoz <kristian.munoz@codurance.com>
2020-02-04 16:06:53 +0100

    Leap year is not divisible by 4 - success

---
+++ src/main/java/LeapYear.java
@@ -0,0 +1,5 @@
+public class LeapYear {
+    public static boolean check(int year) {
+        return false;
+    }
+}
--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+
 public class LeapYearShould {
     @Test
     public void not_be_a_leap_year_when_is_not_divisible_by_4() {
